43d74e74ec7bbc4d68c94bdded3bbf9acd7f9ff6
ARtic <sebastianjvalenzuelavargas@gmail.com>
2026-02-01 20:50:22 -0400

    final version

---
+++ script.js
@@ -0,0 +1,16 @@
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      } else {
+        entry.target.classList.remove("show");
+      }
+    });
+  },
+  { threshold: 0.15 }
+);
+
+reveals.forEach(el => observer.observe(el));
